d106bdea5fef88a38500d52e2ad8dd8f8335aea0
Kyrian <dr.kyrian.onwe@gmail.com>
2022-09-02 14:33:12 -0500

    commit tree e4030d0425b951c0976857da39ce302a27d5a92f

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative './user.rb'
+
+class Student < User
+    
+    KNOWLEDGE = []
+
+    def initialize 
+        @knowledge = KNOWLEDGE
+    end
+    
+    
+    def learn(argument)
+        KNOWLEDGE.append(argument)
+    end
+    
+    def knowledge
+        @knowledge
+    end
 
 end
